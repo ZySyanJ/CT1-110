02fab209e8d6173d32076e9ba6422f4aeb97f647
ZySyanJ <zcj4105@gmail.com>
2024-09-19 11:40:05 -0400

    Upload of P1HW1

---
+++ P1HW1_JohnsonZySyan.py
@@ -0,0 +1,35 @@
+# ZySyan Johnson
+# 9/17/2024
+# P1HW1
+# Input and Output with mathematical expressions
+
+print("------Calculating Exponents------")
+print ()
+print ()
+
+# Get base value (as an integer) from the user
+base_value = int(input("Enter an intger as a base value: "))
+
+# Get exponent value from user
+exponent = int(input("Enter an integer as the exponent: "))
+
+# Raise the base_value to the exponent
+value = base_value ** exponent
+
+print()
+print()
+
+#Display output to the user
+print(base_value, "raised to the power of", exponent, "is", value, "!!")
+
+print()
+print ()
+print("------Addition and Subtraction------")
+print()
+print()
+start_num = int(input("Enter a starting integer: "))
+add_num = int(input("Enter an integer to add: "))
+sub_num = int(input("Enter an integer tosubtract: "))
+print()
+print()
+print(start_num, "+", add_num, "-", sub_num, "is equal to", start_num + add_num - sub_num)
