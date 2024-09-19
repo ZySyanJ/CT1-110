88cc6824127e100b3d831fb60238880b672da423
ZySyanJ <zcj4105@gmail.com>
2024-09-19 11:07:46 -0400

    First upload of P1LAB1

---
+++ P1LAB1_JohnsonZySyan..py
@@ -0,0 +1,13 @@
+ZySyan Johnson
+9/17/2024
+P1LAB1
+Get input from user and display input
+
+Get first name from user
+first_name = input ("Enter your first name")
+
+Get last name from user
+last_name = input ("Enter your last name")
+
+Display info to user
+Print ("Hello", first_name, last_name" + Welcome to CT1-110")
