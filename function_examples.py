f93d2a8ce5bf5e37e7d0804efd0d8abed9335b80
ZySyanJ <zcj4105@gmail.com>
2024-11-12 16:04:02 +0000

    first commit - created file

---
+++ function_examples.py
@@ -0,0 +1,19 @@
+# Create user-defined functions
+
+# Defined a non-value returning function
+def my_animal(name, sound, pounds_food):
+    print(f"The {name} makes a {sound} noise!")
+    print(f"The {name} eats {pound_food} pounds of food a day")
+    print(f"The {name} eats {pound_food * 7} pounds of food a week")
+
+
+# Create a main function - all logic goes here
+def main():
+    print("The main function is executing")
+    print()
+    # Call the my_animal function
+    my_animal("Lion", "rarwwwwrr", 20.5)
+
+# Call the main function
+if __name__ == "__main__": 
+    main()
